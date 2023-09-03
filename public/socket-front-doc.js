c15f4d27f321340cf04b43ac606d5b7711429e0b
AnaBeatriz42 <beatrizsilvarn2016@gmail.com>
2023-09-02 23:39:55 -0300

    mantendo os dados localmente

--- public/socket-front-doc.js
+++ public/socket-front-doc.js
@@ -3,12 +3,13 @@ import { atualizaTextoEditor } from "./doc.js";
 const socket = io(); //emitindo para o backend uma entrada no front 
 
 function selecionandoDoc(nome) {
-     socket.emit("selecionando_doc", nome)
+     socket.emit("selecionando_doc", nome,(texto) =>{
+          atualizaTextoEditor(texto)
+     })
 }
 
 function emitir(dados) {
-     socket.emit("texto_editor", dados);
-    
+     socket.emit("texto_editor", dados);   
 }
 
 socket.on("escrevendo", (res) => { //ouvindo o evento emitido pelo back
